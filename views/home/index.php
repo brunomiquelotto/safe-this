0ae5b715c2c3c607de67225a77c7b65f647910d4
Higor <higoroliveiiira@gmail.com>
2018-04-05 19:58:46 -0300

    Cards com contador de informações finalizados

--- views/home/index.php
+++ views/home/index.php
@@ -1,1 +1,27 @@
 <h1>Bem vindo ao Safe This</h1>
+<div class="container-main">
+    <div class="card-count info">
+        <div>
+            <i class="fa fa-building"></i>
+            <h2>10</h2>
+            <h3>Setores Cadastrados</h3>        
+        </div>
+        
+    </div>
+    <div class="card-count success">
+        <div>
+            <i class="fa fa-list"></i>
+            <h2>10</h2>
+            <h3>Ocorrências Cadastradas</h3>        
+        </div>
+        
+    </div>
+    <div class="card-count danger">
+        <div>
+            <i class="fa fa-user"></i>
+            <h2>10</h2>
+            <h3>Setores Cadastrados</h3>        
+        </div>
+        
+    </div>
+</div>
