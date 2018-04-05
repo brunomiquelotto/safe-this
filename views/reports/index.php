f19a1b09b0dfc5c0b28668a23434e9a010f671d0
Your Name <you@example.com>
2018-04-05 20:45:05 -0300

    alteracoes layout

--- views/reports/index.php
+++ views/reports/index.php
@@ -2,28 +2,30 @@
 
 <div>
     <div>
-       <h3>Filtros</h3>
-       <tr/>
-       <select>
-            <option>ISE</option>
-            <option>Central</option>
-            <option>Engenharia</option>
-       </select>
+        <h3>Filtros</h3>
+        <div class="form-group">
+            <select class="form-control">
+                    <option>ISE</option>
+                    <option>Central</option>
+                    <option>Engenharia</option>
+            </select>
+        </div>
 
-       <select>
-            <option>Em andamento</option>
-            <option>Em analise</option>
-            <option>Finalizado</option>
-        </select>
+        <div class="form-group">
+            <select class="form-control">
+                <option>Em andamento</option>
+                <option>Em analise</option>
+                <option>Finalizado</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <input type="date" class="form-control" placeholder="Período">
+            <input type="date" class="form-control">
+        </div>
 
-       <label for="date">Periodo</label>
-       <input type="date" id="date">
-       <input type="date" id="date">
-
-       
-       <a href="<?=HOME_URI?>/users/create" class="button success ripple mg-t-10">Gerar Relatorio</a>
-       <a href="<?=HOME_URI?>/users/create" class="button success ripple mg-t-10">Gerar PDF</a>
-       <a href="<?=HOME_URI?>/users/create" class="button success ripple mg-t-10">Gerar Imprimir Relatorio Completo</a>
+        <a href="<?=HOME_URI?>/users/create" class="button success ripple mg-t-10">Gerar Relatorio</a>
+        <a href="<?=HOME_URI?>/users/create" class="button success ripple mg-t-10">Gerar PDF</a>
+        <a href="<?=HOME_URI?>/users/create" class="button success ripple mg-t-10">Gerar Imprimir Relatorio Completo</a>
     </div>
     <center>
         <label type="text">Clique em pesquisar para gerar relatorio</label>
