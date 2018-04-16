55ddfcaf6c435898aa3ca0cc62f51ba4ff2fd36d
Silas Caxias <silascaxias@gmail.com>
2018-04-16 15:34:05 -0300

    Ajustes frontend / backend em Relatório
    Conexão com de campos com o banco

--- controllers/reports-controller.php
+++ controllers/reports-controller.php
@@ -1,12 +1,21 @@
 <?php
 
 class ReportsController extends MainController {
-    
+    public function __construct() {
+        parent::__construct();
+        $this->load_model('VwSectorListInfoModel');
+        $this->load_model('OcurrenceStatusModel');
+        $this->load_model('OcurrencesModel');
+        $this->load_model('ShowResultsModel');
+    }
     public function index() {
         $this->ensure_is_logged();
         $this->title = "Relatórios administrativos";
         $this->model = (object)array();
         $this->model->showResults = false;
+        $this->model->reports = VwSectorListInfoModel::all();
+        $this->model->status = OcurrenceStatusModel::all();
+        $this->model->ocurrence = OcurrencesModel::all();
         $this->load_page('reports/index.php');
     }
 
@@ -14,7 +23,7 @@ class ReportsController extends MainController {
         $this->ensure_is_logged();
         $this->title = "Relatórios Administrativos";
         $this->model = (object)array();
-        $this->model->showResults = true;
+        $this->model->showResults = true;        
         $this->load_page('reports/index.php');
     }
 }
--- views/reports/index.php
+++ views/reports/index.php
@@ -3,20 +3,39 @@
 <div>
     <div>
         <div class="form-group">
-            <select class="form-control small">
-                    <option>ISE</option>
-                    <option>Central</option>
-                    <option>Engenharia</option>
+            <select class="form-control small" name="Sector_Id">
+                <option value="0">Selecione um Local</option>
+                <?php foreach($this->model->reports as $place) { ?>
+                    <option value="<?=$place['Sector_Id']?>">
+                        <?=$place['Name']?>
+                    </option>
+                <?php } ?>
+            </select> 
+            <select class="form-control small" name="Ocurrence_Status_Id">
+                <option value="0">Selecione um status</option>
+                <?php foreach($this->model->status as $place) { ?>
+                    <option value="<?=$place['Ocurrence_Status_Id']?>">
+                        <?=$place['Description']?>
+                    </option>
+                <?php } ?>
             </select>
-            <select class="form-control small">
-                <option>Em andamento</option>
-                <option>Em analise</option>
-                <option>Finalizado</option>
+            <select class="form-control small" name="Ocurrence_Id">
+                <option value="0">Data de abertura</option>
+                <?php foreach($this->model->ocurrence as $date) { ?>
+                    <option value="<?=$date['Ocurrence_Id']?>">
+                        <?=$date['Opening_Date'];?>
+                    </option>
+                <?php } ?>
             </select>
-            <input type="date" class="form-control small" placeholder="Período">
-            <input type="date" class="form-control small">
+            <select class="form-control small" name="Ocurrence_Id">
+                <option value="0">Data de Encerramento</option>
+                <?php foreach($this->model->ocurrence as $date) { ?>
+                    <option value="<?=$date['Ocurrence_Id']?>">
+                        <?=$date['Closing_Date'];?>
+                    </option>
+                <?php } ?>
+            </select>
         </div>
-
         <a href="<?=HOME_URI?>/reports/view" class="button info ripple mg-t-10">Gerar Relatorio</a>
         <a href="<?=HOME_URI?>/reports/download" class="button info ripple mg-t-10">Gerar PDF</a>
         <a href="<?=HOME_URI?>/reports/view/complete" class="button info ripple mg-t-10">Gerar Relatorio Completo</a>
