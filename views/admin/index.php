55fb6057a7d38062b789a12c77c18f599ed5c3ad
Higor <higoroliveiiira@gmail.com>
2018-04-16 21:59:24 -0300

    Até que enfim achei a dash nessa porra

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,13 +1,86 @@
 <h1>Bem vindo ao Safe This</h1>
-<div class="card w33 displayIB">
-    <h3>Setores Cadastrados</h3>
-    <p>Aqui vai um contador com os dados do banco e um icone marotasso</p>
+
+<div class="container-main">
+    <div class="card-count info">
+        <div>
+            <i class="fa fa-building"></i>
+            <h2>10</h2>
+            <h3>Setores Cadastrados</h3>        
+        </div>
+        <div class="card-count success">
+            <div>
+                <i class="fa fa-list"></i>
+                <h2>10</h2>
+                <h3>Ocorrências Cadastradas</h3>        
+            </div>
+        </div>
+        <div class="card-count danger">
+            <div>
+                <i class="fa fa-user"></i>
+                <h2>10</h2>
+                <h3>Setores Cadastrados</h3>        
+            </div>
+        </div>
+    </div>
 </div>
-<div class="card w33 displayIB">
-    <h3>Ocorrências abertas</h3>
-    <p>Aqui vai um contador com os dados do banco e um icone marotasso</p>    
+
+<div class="center-all mg-t-50">
+    <h3 class="bolder">Últimas Ocorrências Cadastradas</h3>
 </div>
-<div class="card w33 displayIB">
-    <h3>Ocorrências atendidas</h3>
-    <p>Aqui vai um contador com os dados do banco e um icone marotasso</p>    
+
+<div class="mg-t-10">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Solicitante</th>
+                <th>Descrição</th>
+                <th>Local</th>
+                <th>Status</th>
+                <th>Nível de Urgência</th>
+                <th>Aberto Em</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Bruno Miquelotto</td>
+                <td>Testou</td>
+                <td>Centralzão Meu pau na sua mão</td>
+                <td>Em Andamento</td>
+                <td>Alto</td>
+                <td>05/04/2018</td>
+            </tr> 
+            <tr>
+                <td>Bruno Miquelotto</td>
+                <td>HIHIHIHI PROBLEMINHA</td>
+                <td>Centralzão Meu pau na sua mão</td>
+                <td>Em Andamento</td>
+                <td>Alto</td>
+                <td>05/04/2018</td>
+            </tr> 
+            <tr>
+                <td>Bruno Miquelotto</td>
+                <td>HIHIHIHI PROBLEMINHA</td>
+                <td>Centralzão Meu pau na sua mão</td>
+                <td>Em Andamento</td>
+                <td>Alto</td>
+                <td>05/04/2018</td>
+            </tr>
+            <tr>
+                <td>Bruno Miquelotto</td>
+                <td>HIHIHIHI PROBLEMINHA</td>
+                <td>Centralzão Meu pau na sua mão</td>
+                <td>Em Andamento</td>
+                <td>Alto</td>
+                <td>05/04/2018</td>
+            </tr>
+            <tr>
+                <td>Bruno Miquelotto</td>
+                <td>HIHIHIHI PROBLEMINHA</td>
+                <td>Centralzão Meu pau na sua mão</td>
+                <td>Em Andamento</td>
+                <td>Alto</td>
+                <td>05/04/2018</td>
+            </tr>
+        </tbody> 
+    </table>
 </div>
