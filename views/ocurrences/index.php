f4a1e7926f320ddcee68be44528e5140b50651c4
Silas Caxias <silascaxias@gmail.com>
2018-04-17 20:23:27 -0300

    Tratamento do campo date
    Tratamento do formato do campo Opening_Date.

--- views/ocurrences/index.php
+++ views/ocurrences/index.php
@@ -34,7 +34,7 @@ d<div class="center-all">
 					<td><?=$ocurrence['Id']?></td>
 					<td><?=$ocurrence['Place']?></td>
 					<td><?=$ocurrence['Priority']?></td>
-					<td><?=$ocurrence['Opening_Date']?></td>
+					<td><?=date('d/m/Y H:i:s', strtotime($ocurrence['Opening_Date']))?></td>
 					<td><?=$ocurrence['Files']?></td>
 					<?php if ($this->logged_in): ?>
 					<td>
