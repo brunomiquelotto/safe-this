8915562baf5d2dca76883cafc2857a8bbab14ca4
Bruno Miquelotto <bruno.miquelotto.bm@gmail.com>
2018-04-17 20:05:20 -0300

    adicionado script de criação de view

---
+++ scripts/007-vw_ocurrences.sql
@@ -0,0 +1,18 @@
+CREATE VIEW VW_ocurrences_table AS
+	SELECT
+		oc.Ocurrence_Id as Id,
+		oc.Description,
+		sec.Name as Place,
+		ocs.Description as Status,
+		ocp.Description as Priority,
+		COUNT(ocf.Ocurrence_File_Id) as Files,
+        ocp.Ocurrence_Priority_Id,
+        ocs.Ocurrence_Status_Id,
+        oc.Opening_Date
+	FROM TB_OCURRENCES oc
+		LEFT JOIN TB_OCURRENCE_UPDATES ocu ON ocu.Ocurrence_Id = oc.Ocurrence_Id
+		LEFT JOIN TB_SECTORS sec ON sec.Sector_Id = oc.Sector_Id
+		LEFT JOIN TB_Ocurrence_Priorities ocp ON ocp.Ocurrence_Priority_Id = oc.Ocurrence_Priority_Id
+		LEFT JOIN TB_OCURRENCE_STATUSES ocs ON ocs.Ocurrence_Status_Id = ocu.Ocurrence_Status_Id
+		LEFT JOIN TB_OCURRENCE_FILES ocf ON ocu.Ocurrence_Update_Id = ocf.Ocurrence_Update_Id
+		GROUP BY oc.Ocurrence_Id, oc.Description, ocs.Description;
