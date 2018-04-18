26632e4c2ee33a8443ac30e222616dd12021fd2f
Silas Caxias <silascaxias@gmail.com>
2018-04-18 02:20:03 -0300

    Modificação de cabeçalho de ocorrências
    Foram alterados os titulos e botões de novo e voltar.

--- views/ocurrences/create.php
+++ views/ocurrences/create.php
@@ -1,5 +1,8 @@
-<a href="<?=HOME_URI?>/ocurrences" class="button warning ripple mg-t-10">Voltar</a>
-<h2>Registrar nova ocorrência</h2>
+<div class="center-all">
+    <h1>Registrar nova ocorrência</h1><a href="<?=HOME_URI?>/ocurrences" class="button back small">Voltar</a>
+</div>
+<hr>
+
 <div style="width:50%">
     <form action="<?=HOME_URI?>/ocurrences/save" method="post" enctype="Multipart/form-data">       
         <select class="form-control small" name="Sector_Id">
