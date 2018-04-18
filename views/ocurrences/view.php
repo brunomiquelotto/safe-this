dc3d11f8515c45b53c6a043ca88dba71b5dbea6b
Bruno Miquelotto <bruno.miquelotto.bm@gmail.com>
2018-04-17 22:00:57 -0300

    criação metodo view no controler

---
+++ views/ocurrences/view.php
@@ -0,0 +1,8 @@
+<h3>Detalhes</h3>
+<p><span style="font-weight: bold">Descrição: </span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<p><span style="font-weight: bold">Data: </span> 10/12/1996</p>
