35f89abf469cd7f8877af688ecd454cd8aafa54c
Silas Caxias <silascaxias@gmail.com>
2018-04-18 23:53:38 -0300

    Conexão com o banco!
    OBS: Falta tratar para quando não houver data de fechamento e a questão das pictures.

--- views/ocurrences/view.php
+++ views/ocurrences/view.php
@@ -6,34 +6,35 @@
 <fieldset class="ocurrence-fieldset">
 	<legend><h3>Dados da ocorrência</h3></legend>
 
-		<p class="form-group"><span style="font-weight: bold">Local:</span> Casa do Silas</p>
+		<p class="form-group"><span style="font-weight: bold">Local:</span> <?=$this->model->ocurrence['Place'] ?>.</p>
 
-		<p class="form-group"><span style="font-weight: bold">Descrição: </span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<p class="form-group"><span style="font-weight: bold">Descrição: </span>
+			 <?=$this->model->ocurrence['Description'] ?>
+		</p>
 
 </fieldset>
 
 <fieldset class="ocurrence-fieldset">
 	<legend><h3>Dados do reporter</h3></legend>
 
-		<p class="form-group"><span style="font-weight: bold">Email:</span> silascaxias@gmail.com</p>
+		<p class="form-group"><span style="font-weight: bold">Email:</span>  <?=$this->model->ocurrence['Reporter_Email'] ?>.</p>
 
-		<p class="form-group"><span style="font-weight: bold">CPF: </span>455.970.348-55</p>
+		<p class="form-group"><span style="font-weight: bold">CPF: </span>  <?=$this->model->ocurrence['Reporter_CPF'] ?>.</p>
 
 </fieldset>
 
 <fieldset class="ocurrence-fieldset">
 	<legend><h3>Detalhes da ocorrência</h3></legend>
 
-		<p class="form-group"><span style="font-weight: bold">Local:</span> Casa do Silas</p>
+		<p class="form-group"><span style="font-weight: bold">Local:</span>  <?=$this->model->ocurrence['Place'] ?>.</p>
 
-		<p class="form-group"><span style="font-weight: bold">Data de abertura: </span>10/12/1996</p>
+		<p class="form-group"><span style="font-weight: bold">Data de abertura: </span> 
+			<?=date('d/m/Y H:i:s', strtotime($this->model->ocurrence['Opening_Date']))?>.
+		</p>
 
-		<p class="form-group"><span style="font-weight: bold">Data de fechamento: </span>10/12/1996</p>
+		<p class="form-group"><span style="font-weight: bold">Data de fechamento: </span> 
+			<?=date('d/m/Y H:i:s', strtotime($this->model->ocurrence['Closing_Date']))?>.
+		</p>
 
 </fieldset>
 
@@ -42,7 +43,7 @@
 		<p class="form-group">
 			<center><span style="font-weight: bold">Casa do Silas</span></center>	
 			<a href="/safe-this/views/_uploads/1/img.jpg" >
-				<center><img src="/safe-this/views/_uploads/1/img.jpg" class="ocurrence-image" alt="Descrição de Casa do Silas"></center>	
+				<center><img src="<?=$this->model->ocurrence['Pictures'] ?>" class="ocurrence-image" alt="Descrição de Casa do Silas"></center>	
 			</a>
 		</p>	
 </fieldset>
