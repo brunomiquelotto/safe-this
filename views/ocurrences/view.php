f9656fc929ce6af8f6bd3e6b6f149be108547141
Silas Caxias <silascaxias@gmail.com>
2018-04-20 00:48:16 -0300

    Add cards style in page ocurrence - view.php
    Cards waiting for a new color palette.

--- views/ocurrences/view.php
+++ views/ocurrences/view.php
@@ -3,49 +3,63 @@
 </div>
 <hr>
 <br><h2>Detalhes</h2><br>	
-<fieldset class="ocurrence-fieldset">
-	<legend><h3>Dados da ocorrência</h3></legend>
+<div class="card">
 
-		<p class="form-group"><span style="font-weight: bold">Local:</span> <?=$this->model->ocurrence['Place'] ?>.</p>
+	<div class="container">
+		<fieldset class="ocurrence-fieldset">
+			<legend><header class="view-title"><h3>Dados da ocorrência</h3></header></legend>
 
-		<p class="form-group"><span style="font-weight: bold">Descrição: </span>
-			 <?=$this->model->ocurrence['Description'] ?>
-		</p>
+				<p class="form-group"><span style="font-weight: bold">Local:</span> <?=$this->model->ocurrence['Place'] ?>.</p>
 
-</fieldset>
+				<p class="form-group"><span style="font-weight: bold">Descrição: </span>
+					 <?=$this->model->ocurrence['Description'] ?>
+				</p>
 
-<fieldset class="ocurrence-fieldset">
-	<legend><h3>Dados do reporter</h3></legend>
+		</fieldset>
+	</div>
+</div>
+<div class="card">
+	<div class="container">
+		<fieldset class="ocurrence-fieldset">
+			<legend><header class="view-title"><h3>Dados da Reporter</h3></header></legend>
 
-		<p class="form-group"><span style="font-weight: bold">Email:</span>  <?=$this->model->ocurrence['Reporter_Email'] ?>.</p>
+				<p class="form-group"><span style="font-weight: bold">Email:</span>  <?=$this->model->ocurrence['Reporter_Email'] ?>.</p>
 
-		<p class="form-group"><span style="font-weight: bold">CPF: </span>  <?=$this->model->ocurrence['Reporter_CPF'] ?>.</p>
+				<p class="form-group"><span style="font-weight: bold">CPF: </span>  <?=$this->model->ocurrence['Reporter_CPF'] ?>.</p>
 
-</fieldset>
+		</fieldset>
+	</div>
+</div>
+<div class="card">
+	<div class="container">
+		<fieldset class="ocurrence-fieldset">
+			<legend><header class="view-title"><h3>Detalhes da ocorrência</h3></header></legend>
 
-<fieldset class="ocurrence-fieldset">
-	<legend><h3>Detalhes da ocorrência</h3></legend>
+				<p class="form-group"><span style="font-weight: bold">Local:</span>  <?=$this->model->ocurrence['Place'] ?>.</p>
 
-		<p class="form-group"><span style="font-weight: bold">Local:</span>  <?=$this->model->ocurrence['Place'] ?>.</p>
+				<p class="form-group"><span style="font-weight: bold">Data de abertura: </span> 
+					<?=$this->model->ocurrence['Opening_Date'] ? date('d/m/Y H:i:s', strtotime($this->model->ocurrence['Opening_Date'])) : "Não há" ?>. 
+				</p>
 
-		<p class="form-group"><span style="font-weight: bold">Data de abertura: </span> 
-			<?=$this->model->ocurrence['Opening_Date'] ? date('d/m/Y H:i:s', strtotime($this->model->ocurrence['Opening_Date'])) : "Não há" ?>. 
-		</p>
+				<p class="form-group"><span style="font-weight: bold">Data de fechamento: </span> 
+					<?=$this->model->ocurrence['Closing_Date'] ? date('d/m/Y H:i:s', strtotime($this->model->ocurrence['Closing_Date'])) : "Não há" ?>.
+				</p>
 
-		<p class="form-group"><span style="font-weight: bold">Data de fechamento: </span> 
-			<?=$this->model->ocurrence['Closing_Date'] ? date('d/m/Y H:i:s', strtotime($this->model->ocurrence['Closing_Date'])) : "Não há" ?>.
-		</p>
-
-</fieldset>
-
-<fieldset class="ocurrence-fieldset">
-	<legend><h3>Imagem da ocorrência</h3></legend>
-		<?php foreach($this->model->ocurrence['Pictures'] as $pic) { ?>
-            <figure>
-  				<img src="<?=$pic['src']?>" alt="<?=$pic['title']?>" width="304" height="228">
-  				<figcaption><?=$pic['title']?></figcaption>
-			</figure><br>    
-        <?php } ?>
-</fieldset>
+		</fieldset>
+	</div>
+</div>
+<div class="card">
+	<div class="container">
+		<fieldset class="ocurrence-fieldset">
+			<legend><header class="view-title"><h3>Imagens da ocorrência</h3></header></legend>
+				<?php foreach($this->model->ocurrence['Pictures'] as $pic) { ?>
+		            <figure>
+		  				<img src="<?=$pic['src']?>" alt="<?=$pic['title']?>" width="304" height="228">
+		  				<figcaption><?=$pic['title']?></figcaption>
+					</figure><br>    
+		        <?php } ?>
+		</fieldset>
+	</div>
+</div>
 		
 <a href="<?=HOME_URI?>/ocurrences" class="button warning ripple mg-t-10">Voltar</a>
