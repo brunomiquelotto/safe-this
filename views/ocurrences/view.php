6adae080c51c739bb9eaaae91fc04fff2c176dcf
Silas da Silva Caxias <silascaxias@gmail.com>
2018-05-12 17:15:27 -0300

    view att

--- views/ocurrences/view.php
+++ views/ocurrences/view.php
@@ -43,48 +43,21 @@
 		<h3>Atualizações</h3>
 	</header>
 	<section>
+		 <?php foreach($this->model->updates as $update) { ?>
 		<div class="update-card">
 			<header>
-				<span class="title">Em andamento</span>
-				<span class="subtitle">Responsável: Silas Caxias</span>	
+
+				<span class="title"><?=$update['Ocurrence_Status_Id']?></span>
+				<span class="subtitle"><?php=($update['Responsible'] ? "Sem resposável" : "$update['Responsible']")?></span>	
 			</header>
 			<section>
-				<span class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+				<span class="content">
+					
+					<?=$update['Status_Feedback']?>
+				</span>
 			</section>
 		</div>
-		<div class="update-card">
-			<header>
-				<span class="title">Em andamento</span>
-				<span class="subtitle">Responsável: Silas Caxias</span>	
-			</header>
-			<section>
-				<span class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
-			</section>
-		</div>
-		<div class="update-card">
-			<header>
-				<span class="title">Em andamento</span>
-				<span class="subtitle">Responsável: Silas Caxias</span>	
-			</header>
-			<section>
-				<span class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
-			</section>
-		</div>
+		<?php } ?>
 	</section>
 </div>
 				
