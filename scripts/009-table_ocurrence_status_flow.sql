9895d51414c9d6423447b7e1ef5ff17eb54928ac
Bruno Miquelotto <bruno.miquelotto.bm@gmail.com>
2018-05-12 21:03:45 -0300

    Correção bugs listagem

--- scripts/009-table_ocurrence_status_flow.sql
+++ scripts/009-table_ocurrence_status_flow.sql
@@ -3,6 +3,7 @@ CREATE TABLE `safe_this_dev`.`tb_ocurrence_status_flow` (
   `Current_Status` INT NOT NULL,
   `Next_Status` INT NOT NULL,
   `Description` VARCHAR(45) NOT NULL,
+  'Default_Message' VARCHAR(255) NOT NULL
   PRIMARY KEY (`OcurrenceStatusFlowId`),
   INDEX `FK_TB_STATUSES_TB_FLOW_idx` (`Current_Status` ASC),
   INDEX `FK_TB_STATUSES_TB_FLOW_NEXT_idx` (`Next_Status` ASC),
@@ -17,9 +18,9 @@ CREATE TABLE `safe_this_dev`.`tb_ocurrence_status_flow` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
-INSERT INTO TB_OCURRENCE_STATUS_FLOW (Current_Status, Next_Status, Description)
+INSERT INTO TB_OCURRENCE_STATUS_FLOW (Current_Status, Next_Status, Description, Default_Message)
 VALUES
-(1, 2, 'Rejeitar'),
-(1, 3, 'Aprovar'),
-(3, 4, 'Começar'),
-(4, 5, 'Finalizar')
+(1, 2, 'Rejeitar', 'A ocorrência registrada não foi aceita.'),
+(1, 3, 'Aprovar', 'A ocorrência foi aprovada.'),
+(3, 4, 'Começar', 'Iniciando os trabalhos na ocorrência'),
+(4, 5, 'Finalizar', 'A ocorrência foi finalizada.');
